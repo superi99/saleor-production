4f53a8bcf88a6b1ced7aaf5b87b5c7c295f596be
Ngo Minh Thang <ngominhthang@Ngos-MacBook-Pro.local>
2021-03-23 20:52:41 +0700

    first commit

---
+++ dist/service-worker.js
@@ -0,0 +1,32 @@
+importScripts("/precache-manifest.849853c6ed6dee2331508d7f9307dbf8.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* eslint-disable no-undef,no-restricted-globals */
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+workbox.core.setCacheNameDetails({
+  prefix: "saleor-store-front",
+});
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+workbox.routing.registerRoute(
+  new RegExp("^http.*(?:png|gif|jpg|jpeg|webp|svg|csv|pdf)"),
+  new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL("/index.html"),
+  {
+    blacklist: [
+      new RegExp("/graphql"),
+      new RegExp("/dashboard"),
+      new RegExp("/media/export_files"),
+      new RegExp("/plugins"),
+      new RegExp("/storybook"),
+      new RegExp("/__"), // used by cypress tests runner
+    ],
+  }
+);
+
